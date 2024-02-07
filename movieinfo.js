2fd610699a446555df04bfd11ab931a5389956c5
destro <irtizasafdar@hotmail.co.uk>
2024-02-07 18:43:29 +0000

    integration in progress

--- movieinfo.js
+++ movieinfo.js
@@ -1,154 +1,208 @@
-const movieContainer = document.getElementById('modal-body')
-const searchMoviesBtn = document.getElementById('searchBtn')
+const movieContainer = document.getElementById('modal-body');
+const searchMoviesBtn = document.getElementById('searchBtn');
+const searchResultsModal = document.getElementById('searchResultsModal');
 
-const watchLibraryContainer = document.getElementById('displayContainer')
-const watchLibraryBtn = document.getElementById('watch-library-button')
-const watchLibraryheading = document.getElementById('w-l-heading')
-const watchLibrarySlogan = document.getElementById('w-l-info')
+const watchLibraryContainer = document.getElementById('displayContainer');
+const watchLibraryBtn = document.getElementById('watch-library-button');
+const watchLibraryheading = document.getElementById('w-l-heading');
+const watchLibrarySlogan = document.getElementById('w-l-info');
+const saveChangesBtn = document.getElementById('save-changes-btn');
 
-var searchValue = document.getElementById('searchValue')
-var savedMovies = []
+const defaultImageURL = 'defaultmovieimage.jpg';
+// Retrieve saved movies from local storage
+var savedmovies = JSON.parse(localStorage.getItem('savedmovies')) || [];
+var searchValue = document.getElementById('searchValue');
 
 // fetching results, opening a modal, displaying the results
 searchMoviesBtn.addEventListener('click', function(e) {
-  e.preventDefault()
-  console.log(e.target)
-  var queryString = `https://www.omdbapi.com/?s=${searchValue.value}&i=tt3896198&apikey=9a161259`
-  fetch(queryString).then(res => {
-    return res.json()
-  }).then(data => {
-    // console.log(data)
-    let results = data.Search
-    const clonedArray = [...results]
-    console.log(clonedArray)
-    movieContainer.innerHTML = ''
-    
-    clonedArray.forEach((item) => {
-      let movieDiv = document.createElement('div')
-      movieDiv.innerHTML = `
-        <div class='card d-flex justify-content-center align-items-center'>
-          ${item.Title}
-          <img src='${item.Poster}' alt='${item.Title}'>
-          <button id='${item.imdbID}'class='saveBook btn btn-primary' type='submit' value='${item.imdbID}'>
-          <!---<i class='fa-regular fa-bookmark' value='${item.imdbID}'></i>--->
-          bookmark
-          </button>
-        </div>
-      `
-      movieContainer.append(movieDiv)
-      let bookmarkBtn = document.getElementById(`${item.imdbID}`)
-      bookmarkBtn.addEventListener('click', function(e) {
-        e.preventDefault()
-        console.log(e.target.value, item.imdbID)
-        //push the movie to the savedmovies list - when the current bookmark id is the same as the movie id
-        if (e.target.value === item.imdbID) {
-          savedMovies.push(item)
-          let currentElement = item
-          watchLibraryBtn.addEventListener('click', function() {
-            displayWatchList(currentElement)
-          })
-            
-        }
-        
-        // return savedMovies
-      })
+  e.preventDefault();
+
+  // Check if the searchValue is empty
+  if (!searchValue.value.trim()) {
+    alert('Please enter a search value.');
+    return;
+  }
+
+  var queryString = `https://www.omdbapi.com/?s=${searchValue.value}&i=tt3896198&apikey=9a161259&plot=full&r=json`;
+  fetch(queryString)
+    .then((res) => res.json())
+    .then((data) => {
+      let results = data.Search;
+      movieContainer.innerHTML = '';
+
+      results.forEach((item) => {
+        console.log(item);
+        let movieDiv = document.createElement('div');
+        movieDiv.innerHTML = `
+          <div class='card d-flex justify-content-center align-items-center'>
+            ${item.Title}
+            <img class='img-formatting' src='${item.Poster !== 'N/A' ? item.Poster : defaultImageURL}' alt='${item.Title}'>
+            <p>Plot: ${item.Plot}</p>
+            <p>Ratings:</p>
+            <p>TMDB Rating: ${item.Ratings && item.Ratings[0] ? item.Ratings[0].Value : 'N/A'}</p>
+
+            <button id='${item.imdbID}' class='btn fa-large fa-bookmark' type='submit' value='${item.imdbID}'></button>
+          </div>
+        `;
+        movieContainer.append(movieDiv);
+
+        let bookmarkBtn = document.getElementById(`${item.imdbID}`);
+        bookmarkBtn.addEventListener('click', function(e) {
+          e.preventDefault();
+          if (e.target.value === item.imdbID) {
+            // Save the movie to local storage
+            savedmovies.push(item);
+            savedmovies = checkDuplicates(savedmovies);
+            localStorage.setItem('savedmovies', JSON.stringify(savedmovies));
+            renderSavedMovie();
+          }
+        });
+      });
     })
-  }).catch(err => console.log(err))
-  // return savedMovies
-})
+    .catch((err) => console.log(err));
+});
 
-// watchLibraryBtn.addEventListener('click', function(e) {
-//   console.log(e.target)
-//   displayWatchList(savedMovies)
-// })
 
-function displayWatchList(item) {
-  // hide the text and view library button
-  showWatchLibrary()
-  // add flexbox to watchlibrary container
-  watchLibraryContainer.classList.add('flexContainer')
-  // loop through movies array and display them within the displayContainer
-  // movies.forEach((item) => {
-    let div = document.createElement('div')
-    div.classList.add('cardbody')
-    div.innerHTML = `
-      <p class="card-text">
-        This is a wider card with supporting text below as a natural
-        lead-in to additional content. This content is a little bit
-        longer.
-      </p>
+function addToList(imdbID, title, poster, plot, tmdbRating) {
+  // Check if the movie is already in the watch library
+  if (savedmovies.some(movie => movie.imdbID === imdbID)) {
+    alert('This movie is already in your watch library.');
+    return;
+  }
 
-      <img src='${item.Poster}' class='img-formatting'>
-      <h5>${item.Title}</h5>
-      <button class='btn btn-primary'>Remove</button>
-    `
-    watchLibraryContainer.append(div)
-  // })
-  // console.log(movies)
-  
+  // Create a movie object
+  const movie = {
+    imdbID: imdbID,
+    Title: title,
+    Poster: poster,
+    Plot: plot,
+    Ratings: [{ Source: 'TMDB', Value: tmdbRating }]
+  };
+
+  // Save the movie to local storage
+  savedmovies.push(movie);
+  savedmovies = checkDuplicates(savedmovies);
+  localStorage.setItem('savedmovies', JSON.stringify(savedmovies));
+
+  // Render the updated watch library
+  renderSavedMovie();
 }
 
+watchLibraryBtn.addEventListener('click', function() {
+  showWatchLibrary();
+  renderSavedMovie();
+});
 
+saveChangesBtn.addEventListener('click', function() {
+  var modal = bootstrap.Modal.getInstance(searchResultsModal); // Retrieve the modal instance
+  modal.hide();
+});
 
-// `<div class="card-body">
-// <p class="card-text">
-//   This is a wider card with supporting text below as a natural
-//   lead-in to additional content. This content is a little bit
-//   longer.
-// </p>
-// <div
-//   class="d-flex justify-content-between align-items-center"
-// >
-//   <div class="btn-group">
-//     <button
-//       type="button"
-//       class="btn btn-sm btn-outline-secondary"
-//     >
-//       View
-//     </button>
-//     <button
-//       type="button"
-//       class="btn btn-sm btn-outline-secondary"
-//     >
-//       Edit
-//     </button>
-//   </div>
-//   <small class="text-body-secondary">9 mins</small>
-// </div>
-// </div>`
+function renderSavedMovie() {
+  // Clear existing content
+  watchLibraryContainer.innerHTML = '';
+
+  savedmovies.forEach(function(movie) {
+    let savedmovieDiv = document.createElement('div');
+    savedmovieDiv.className = 'card mb-3';
+    savedmovieDiv.style.maxWidth = '300px';
+
+    // Use the movie poster if available, otherwise use the default image
+    const imageUrl = movie.Poster !== 'N/A' ? movie.Poster : defaultImageURL;
+
+    savedmovieDiv.innerHTML =
+      `<img src='${imageUrl}' class='card-img-top' alt='${movie.Title}'>
+      <div class='card-body'>
+        <h5 class='card-title'>${movie.Title}</h5>
+        <p class='card-text'>
+          This is a wider card with supporting text below as a natural
+          lead-in to additional content. This content is a little bit
+          longer.
+        </p>
+        <p>TMDB Rating: ${movie.Ratings && movie.Ratings[0] ? movie.Ratings[0].Value : 'N/A'}</p>
+        <button class='btn btn-primary' onclick="removeMovie('${movie.imdbID}')">Remove</button>
+      </div>`;
+
+    watchLibraryContainer.appendChild(savedmovieDiv);
+  })  
+}
+
+function removeMovie(imdbID) {
+  // Remove the movie from savedmovies array
+  savedmovies = savedmovies.filter((movie) => movie.imdbID !== imdbID);
+  // Update local storage
+  localStorage.setItem('savedmovies', JSON.stringify(savedmovies));
+  // Render the updated list
+  renderSavedMovie();
+}
+
+function checkDuplicates(arr) {
+  return arr.filter((value, index) => arr.findIndex((movie) => movie.imdbID === value.imdbID) === index);
+}
 
 function showWatchLibrary() {
-  // hide the headings and buttons 
-  watchLibraryheading.classList.add('hide')
-  watchLibrarySlogan.classList.add('hide')
-  watchLibraryBtn.classList.add('hide')
+  watchLibraryheading.classList.add('hide');
+  watchLibrarySlogan.classList.add('hide');
+  watchLibraryBtn.classList.add('hide');
 }
 
-// // creating the card for how each movie search result should look when displayed
-// function createCard(div, item) {
-//   div.classList.add('card', 'd-flex', 'justify-content-center', 'align-items-center')
-//   let img = document.createElement('img')
-//   img.src = item.Poster
-//   img.alt = item.Title
-//   let div2 = document.createElement('div')
-//   div2.classList.add('card-body', 'd-flex', 'justify-content-center', 'align-items-center')
-//   let h5 = document.createElement('h5')
-//   h5.classList.add('card-title', 'text-center')
-//   h5.textContent = item.Title
-//   let p = document.createElement('p')
-//   p.classList.add('card-text')
-//   p.textContent = item.Plot
-//   let bookmark = document.createElement('i')
-//   bookmark.innerHTML = "<i class='fa-regular fa-bookmark'></i>"
-//   bookmark.addEventListener('click', bookMarkEvent)
-//   // btn.classList.add('btn', 'btn-primary')
-//   div2.append(h5, p, bookmark)
-//   div.append(img, div2)
-//   // bookmark.addEventListener('click', bookMarkEvent)
-//   movieContainer.append(div)
-//   return movieContainer
-// }
+// TMDb API Key
+const apiKey = 'c765a205274e5ae2e4ea7651076c9be2';
+// Function to fetch top movies or top series based on the category parameter
+async function fetchTopContent(category) {
+  let endpoint;
+  if (category === 'movies') {
+    endpoint = 'movie/top_rated';
+  } else if (category === 'series') {
+    endpoint = 'tv/top_rated';
+  }
+  const response = await fetch(`https://api.themoviedb.org/3/${endpoint}?api_key=${apiKey}`);
+  const data = await response.json();
+  return data.results;
+}
 
+// Fetch top content and render cards
+async function renderTopContent(category) {
+  const topContent = await fetchTopContent(category);
+  for (let i = 1; i <= 12; i++) {
+    const content = topContent[i - 1];
+    const cardElement = document.getElementById(`card${i}`);
+    const cardContent = `
+      <div class="col-md-4 mb-4">
+        <div class="card shadow-sm" style="width: 12rem;">
+          <img src="https://image.tmdb.org/t/p/w500${content.poster_path}" class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+          <div class="card-body">
+            <h5 class="card-title">
+              ${content.title || content.name}
+            </h5>
+            <div class="d-flex justify-content-between align-items-center">
+              <button type="button" class="btn btn-sm btn-outline-secondary" onclick="toggleOverview('card${i}')">View</button>
+              <button type="button" class="btn btn-sm btn-outline-secondary" onclick="addToList('${content.id}', '${content.title || content.name}', '${content.poster_path}', '${content.overview}', '${content.vote_average}')">Add to List</button>
+            </div>
+            <p class="card-text overview" style="display: none;">${content.overview}</p>
+            <p class="card-text">Rating: ${content.vote_average}</p>
+          </div>
+        </div>
+      </div>
+    `;
+    cardElement.innerHTML = cardContent;
+  }
+}
 
+// Function to add a movie to the list
+document.getElementById('topMoviesButton').addEventListener('click', function () {
+  renderTopContent('movies');
+});
 
+// function to add series to the list
+document.getElementById('topSeriesButton').addEventListener('click', function () {
+  renderTopContent('series');
+});
 
+// view the overview of the movie
+function toggleOverview(cardId) {
+  const overviewElement = document.querySelector(`#${cardId} .card-text.overview`);
+  overviewElement.style.display = overviewElement.style.display === 'none' ? 'block' : 'none';
+}
+
+// window.onload = renderMovieCards;
